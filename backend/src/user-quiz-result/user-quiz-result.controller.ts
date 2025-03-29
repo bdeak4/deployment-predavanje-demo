9f276b835be3413f0d064841e149cb68625a81a3
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-03-29 23:05:13 +0100

    Add Api endpoints to user-quiz-result.controller

--- backend/src/user-quiz-result/user-quiz-result.controller.ts
+++ backend/src/user-quiz-result/user-quiz-result.controller.ts
@@ -10,36 +10,58 @@ import {
 import { UserQuizResultService } from './user-quiz-result.service';
 import { CreateUserQuizResultDto } from './dto/create-user-quiz-result.dto';
 import { UpdateUserQuizResultDto } from './dto/update-user-quiz-result.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@Controller('user-quiz-result')
+@ApiTags('User Quiz Result')
+@Controller('result')
 export class UserQuizResultController {
   constructor(private readonly userQuizResultService: UserQuizResultService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new quiz result' })
+  @ApiResponse({ status: 201, description: 'Quiz result successfully created' })
+  @ApiResponse({ status: 400, description: 'Invalid input' })
+  @ApiBody({ type: CreateUserQuizResultDto })
   create(@Body() createUserQuizResultDto: CreateUserQuizResultDto) {
     return this.userQuizResultService.create(createUserQuizResultDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all quiz results' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of quiz results returned successfully',
+  })
   findAll() {
     return this.userQuizResultService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a quiz result by ID' })
+  @ApiResponse({ status: 200, description: 'Quiz result found' })
+  @ApiResponse({ status: 404, description: 'Quiz result not found' })
   findOne(@Param('id') id: string) {
-    return this.userQuizResultService.findOne(+id);
+    return this.userQuizResultService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a quiz result by ID' })
+  @ApiResponse({ status: 200, description: 'Quiz result updated successfully' })
+  @ApiResponse({ status: 400, description: 'Invalid input' })
+  @ApiResponse({ status: 404, description: 'QueQuiz result not found' })
+  @ApiBody({ type: UpdateUserQuizResultDto })
   update(
     @Param('id') id: string,
     @Body() updateUserQuizResultDto: UpdateUserQuizResultDto,
   ) {
-    return this.userQuizResultService.update(+id, updateUserQuizResultDto);
+    return this.userQuizResultService.update(id, updateUserQuizResultDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a quiz result by ID' })
+  @ApiResponse({ status: 200, description: 'Quiz result deleted successfully' })
+  @ApiResponse({ status: 404, description: 'Quiz result not found' })
   remove(@Param('id') id: string) {
-    return this.userQuizResultService.remove(+id);
+    return this.userQuizResultService.remove(id);
   }
 }
