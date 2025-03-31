2183cadbf366bca7a606b083cf421690085c7f79
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-03-31 14:43:41 +0200

    Add category.service validation

--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -6,6 +6,14 @@ import { DatabaseService } from 'src/database/database.service';
 export class CategoryService {
   constructor(private readonly databaseService: DatabaseService) {}
   async create(createCategoryDto: CreateCategoryDto) {
+    const existingCategoryName = await this.databaseService.category.findUnique(
+      {
+        where: { name: createCategoryDto.name },
+      },
+    );
+
+    if (existingCategoryName) throw new Error('Category name already exists');
+
     return this.databaseService.category.create({
       data: createCategoryDto,
     });
@@ -16,12 +24,30 @@ export class CategoryService {
   }
 
   async findOne(id: string) {
-    return this.databaseService.category.findUnique({
+    const category = await this.databaseService.category.findUnique({
       where: { id },
     });
+
+    if (!category) throw new Error('Category not found');
+
+    return category;
   }
 
   async update(id: string, updateCategoryDto: CreateCategoryDto) {
+    const category = await this.databaseService.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) throw new Error('Category not found');
+
+    const existingCategoryName = await this.databaseService.category.findUnique(
+      {
+        where: { name: updateCategoryDto.name },
+      },
+    );
+
+    if (existingCategoryName) throw new Error('Category name already exists');
+
     return this.databaseService.category.update({
       where: { id },
       data: updateCategoryDto,
@@ -29,6 +55,12 @@ export class CategoryService {
   }
 
   async remove(id: string) {
+    const category = await this.databaseService.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) throw new Error('Category not found');
+
     return this.databaseService.category.delete({
       where: { id },
     });
