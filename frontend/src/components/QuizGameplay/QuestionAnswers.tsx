7d4505b37803504e5e76ed179c25139156959c24
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-04-10 00:18:46 +0200

    Add Quiz gameplay functionallity

--- frontend/src/components/QuizGameplay/QuestionAnswers.tsx
+++ frontend/src/components/QuizGameplay/QuestionAnswers.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import { useQuestionAnswers } from "../../api/useQuestionAnswers";
 
 type QuestionAnswers = {
@@ -7,31 +8,58 @@ type QuestionAnswers = {
   questionId: string;
 };
 
+type QuestionAnswerProps = {
+  questionId: string;
+  questionType: string;
+  setQuestionCount: React.Dispatch<React.SetStateAction<number>>;
+  setScore: React.Dispatch<React.SetStateAction<number>>;
+};
+
 export default function QuestionAnswers({
   questionId,
   questionType,
-}: {
-  questionId: string;
-  questionType: string;
-}) {
+  setQuestionCount,
+  setScore,
+}: QuestionAnswerProps) {
+  const typedAnswerRef = useRef<HTMLInputElement | null>(null);
   const { data, isFetching, isError, error } = useQuestionAnswers(questionId);
 
-  if (questionType === "MULTIPLE_CHOICE") {
-    return (
-      <div>{data && data.map((a: QuestionAnswers) => <p>{a.text}</p>)}</div>
-    );
-  } else if (questionType === "TRUE_FALSE") {
+  const handleMultipleChoiceClick = (isCorrect: boolean) => {
+    setQuestionCount((prev) => prev + 1);
+
+    if (isCorrect) {
+      setScore((prev) => prev + 1);
+    }
+  };
+
+  const handleAnswerSubmit = () => {
+    setQuestionCount((prev) => prev + 1);
+    if (
+      typedAnswerRef.current?.value.toLowerCase() === data[0].text.toLowerCase()
+    ) {
+      setScore((prev) => prev + 1);
+    }
+  };
+
+  if (questionType === "MULTIPLE_CHOICE" || questionType === "TRUE_FALSE") {
     return (
       <div>
-        <p>TRUE</p>
-        <p>FALSE</p>
+        {data &&
+          data.map((a: QuestionAnswers) => (
+            <button
+              key={a.id}
+              onClick={() => handleMultipleChoiceClick(a.isCorrect)}
+            >
+              {a.text}
+            </button>
+          ))}
       </div>
     );
   } else if (questionType === "FILL_IN_THE_BLANK") {
     return (
       <div>
-        <input type="text" />
-        <button>Submit</button>
+        <input type="text" ref={typedAnswerRef} />
+        <button onClick={handleAnswerSubmit}>Submit</button>
       </div>
     );
   }
--- frontend/src/components/QuizGameplay/QuizGameplay.tsx
+++ frontend/src/components/QuizGameplay/QuizGameplay.tsx
@@ -3,7 +3,8 @@ import { useQuizQuestionsQuery } from "../../api/useQuizQuestionsQuery";
 import QuestionAnswers from "./QuestionAnswers";
 
 export default function QuizGameplay({ quizId }: { quizId: string }) {
-  const [questionCount, setQuestionCount] = useState(1);
+  const [score, setScore] = useState<number>(0);
+  const [questionCount, setQuestionCount] = useState<number>(1);
   const { data, isFetching, isError, error } = useQuizQuestionsQuery(quizId);
   let currentQuestion = null;
 
@@ -11,14 +12,30 @@ export default function QuizGameplay({ quizId }: { quizId: string }) {
     currentQuestion = data[questionCount - 1];
   }
 
+  if (questionCount === 6) {
+    return (
+      <div>
+        <p>
+          Your score was: {score}/{questionCount - 1}
+        </p>
+      </div>
+    );
+  }
+
   return (
     <>
       {currentQuestion ? (
         <div>
+          <p>Score: {score}</p>
+          <p>
+            Question: {questionCount}/{data.length}
+          </p>
           <p>{currentQuestion.text}</p>
           <QuestionAnswers
             questionId={currentQuestion.id}
             questionType={currentQuestion.type}
+            setQuestionCount={setQuestionCount}
+            setScore={setScore}
           />
         </div>
       ) : (
